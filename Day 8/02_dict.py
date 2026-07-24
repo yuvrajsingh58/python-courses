58f064b56269abc81f91971a6050357ebd8fe0eb
yuvrajsingh58 <yuvrajsingh912568@gmail.com>
2026-07-24 14:04:40 +0530

    Update Day 8

---
+++ Day 8/02_dict.py
@@ -0,0 +1,12 @@
+student = {
+    "name": "Yuvraj",
+    "age": 20,
+    "city": "Kannauj"
+    
+}
+
+print(student)
+
+print(student["name"])
+print(student["age"])
+print(student["city"])
---
+++ Day 8/03_set.py
@@ -0,0 +1,3 @@
+numbers = {1,1,2,3,4,4,5,5}
+
+print(numbers)
